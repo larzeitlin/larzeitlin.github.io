d250d79b77a4cfde6806bb831ba38e20d8d53400
Looke Z <luke@Lookes-MacBook-Pro.local>
2023-11-21 21:47:31 +0000

    more unstyle tweaks

--- build-site.el
+++ build-site.el
@@ -6,27 +6,23 @@
 
 (require 'ox-publish)
 
-(setq org-html-validation-link nil
-      org-html-head-include-scripts nil
-      org-html-head-include-default-style nil
-      org-html-head (concat
-		     "<link rel =\"stylesheet\" type= \"text/css\" href=\"./styles.css\">"
-		     "<link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>\">"))
-
 (setq org-html-htmlize-output-type 'css)
-
 
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
        (list
 	"orgfiles"
+	:html-head-extra (concat
+		     "<link rel =\"stylesheet\" type= \"text/css\" href=\"./styles.css\">"
+		     "<link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>\">") 
         :recursive t
+	:html-head-include-default-style t
         :base-directory "./content"
         :publishing-directory "./"
         :publishing-function 'org-html-publish-to-html
 	:with-author nil
-	:include-toc nil
+	:html-head "<header><a href=\"./\">home</a></header>"
         :section-numbers nil
 	:time-stamp-file nil)))
 
