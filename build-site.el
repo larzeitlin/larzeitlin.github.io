460bb867eb0dcef9575cd7f1b75b5b04ce340fec
Looke Z <luke@lookes-mbp.home>
2024-12-30 01:55:39 +0000

    The Ultimate Charm story

--- build-site.el
+++ build-site.el
@@ -10,22 +10,20 @@
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-       (list
-	"orgfiles"
-	:html-head (concat
-"<link rel=\"stylesheet\" type= \"text/css\" href=\"./htmlize.css\">"
-"<link rel=\"stylesheet\" type= \"text/css\" href=\"./styles.css\">"
-"<link rel=\"icon\"
+      (list (list "orgfiles"
+	 :html-head (concat
+		     "<link rel=\"stylesheet\" type= \"text/css\" href=\"./htmlize.css\">"
+		     "<link rel=\"stylesheet\" type= \"text/css\" href=\"./styles.css\">"
+		     "<link rel=\"icon\"
        href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>\">"
-"<header>
+		     "<header>
 | <a href=\"./\">home</a>
 | <a href=\"#top\">top</a>
 | <a href=\"https://github.com/larzeitlin\">github</a>
 |
 </header>
 ") 
-
+	 
 	:recursive t
 	:html-head-include-default-style t
         :base-directory "./content"
